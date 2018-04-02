b0301fc8524988745776c8d4a2596853760d74d2
miroslavasimova <miroslava_simova@abv.bg>
2018-04-02 15:48:42 +0300

    hw1

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,28 @@
+# a
+x <- c(4, 1, 1, 4)
 
+#b
+y <- c(1, 4)
+
+#c
+x - y
+#It is the difference between the elements of vector x and elements of vector y
+
+#d
+s <- c(x, y)
+
+
+#e
+rep(s, 10)
+
+length(s)
+
+# f
+rep(s, each = 3)
+
+# g) 
+ seq(7, 21, by=1)
+7:21
+
+# h
+length(7:21)
